e113eac9cf1fb0178b29ce5059b6728c3b0335aa
Sangamesh <sangameshvenu@gmail.com>
2021-06-24 22:33:46 +0530

    intermediate saving

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,51 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MyApp());
-}
+// void main(){
+//   runApp(MyApp());
+// }
 
-class MyApp extends StatelessWidget{
-  Widget build(BuildContext context){
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  var questionIndex = 0;
+  Widget build(BuildContext context) {
+    void answerPrinter(String value) {
+      print("$value clicked!");
+    }
+
+    void answerPrinter1() {
+      print("Answer chosen");
+    }
+
+    var questions = <String>[
+      "Favourite colour?",
+      "Favourite animal",
+    ];
+
     return MaterialApp(
-      home: Text("Hello World!")
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hello World App"),
+        ),
+        body: Column(children: <Widget>[
+          Text(
+            questions[questionIndex],
+          ),
+          Row(
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: answerPrinter1,
+                child: Text("Answer1"),
+              ),
+              ElevatedButton(
+                onPressed: () => answerPrinter("Answer2"),
+                child: Text("Answer2"),
+              )
+            ],
+          )
+        ]),
+      ),
     );
   }
 }
